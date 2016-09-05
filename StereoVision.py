43ca8d119df01f2916a4c3133c619f01a87cbf78
Georgi Dikov <gvdikov@gmail.com>
2016-09-05 12:43:38 +0200

    no viz

--- StereoVision.py
+++ StereoVision.py
@@ -1,9 +1,18 @@
+###
+# Date: August 2016
+# Author: Georgi Dikov
+# email: gvdikov93@gmail.com
+###
+
 import CooperativeNetwork as net
 import Retina as ret
 import ExternalInputReader as eir
 import Simulation as sim
+import Visualizer as vis
 
 if __name__ == "__main__":
+
+    experiment_name = "Test"
 
     # Setup the simulation
     Simulation = sim.SNNSimulation(simulation_time=100)
@@ -23,16 +32,26 @@ if __name__ == "__main__":
     # Create a cooperative network for stereo vision from retinal disparity
     SNN_Network = net.CooperativeNetwork(retinae={'left': RetinaL, 'right': RetinaR},
                                          max_disparity=3,
-                                         record_spikes=True, visualize_spikes=True, experiment_name="Test")
+                                         record_spikes=True,
+                                         experiment_name=experiment_name)
 
     # Start the simulation
     Simulation.run()
 
-    # Fetch the results and visualize them
+    # Store the results in a file
     disparities = SNN_Network.get_spikes(sort_by_time=True, save_spikes=True)
-    SNN_Network.visualizer.disparity_histogram(over_time=True, save_figure=True)
 
     # Finish the simulation
     Simulation.end()
+
+    # Visualize the results (disparity histograms and 3D scatter animation)
+   # network_dimensions = SNN_Network.get_network_dimensions()
+   # Results = vis.Visualizer(network_dimensions=network_dimensions,
+                             experiment_name=experiment_name,
+                             spikes_file="./spikes/{0}_0.dat".format(experiment_name))
+   # Results.disparity_histogram(over_time=True, save_figure=True)
+   # Results.scatter_animation(dimension=3, save_animation=True, rotate=True)
+
+
 
 
