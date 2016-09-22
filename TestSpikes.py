11c6ec1898c7d90e6f5354d30a02fadcdf037dfe
Georgi Dikov <gvdikov@gmail.com>
2016-09-22 18:30:15 +0200

    Update TestSpikes.py

--- TestSpikes.py
+++ TestSpikes.py
@@ -6,9 +6,14 @@ ps.setup(timestep=0.2, min_delay=0.2, max_delay=10 * 0.2, threads=4)
 
 pixel_r = ps.Population(n, ps.SpikeSourceArray, {'spike_times': [[10, 30], [1000], [1000]]}, label="test_input_r", structure=ps.Line())
 pixel_l = ps.Population(n, ps.SpikeSourceArray, {'spike_times': [[10, 30], [1000], [1000]]}, label="test_input_l", structure=ps.Line())
+print(pixel_r)
+print(pixel_r[3)
+print(pixel_l)
 
 collector = ps.Population(n, ps.IF_curr_exp, {'tau_syn_E': 2.0, 'tau_syn_I': 2.0, 'tau_m': 2.07, 'v_reset': -90.0}, label="collector")
 blockers = ps.Population(n * 2, ps.IF_curr_exp, {'tau_syn_E': 2.0, 'tau_syn_I': 2.0, 'tau_m': 2.07, 'v_reset': -84.0}, label="blockers")
+print(collector)
+print(blockers)
 
 collector.record()
 blockers.record()
@@ -18,8 +23,8 @@ ps.Projection(blockers, collector, ps.FromListConnector(connList), target='inhib
 
 connListRetLBlockerL = [(0, 0, 22.5, 0.2), (1, 1, 22.5, 0.2), (2, 2, 22.5, 0.2), (3, 3, 22.5, 0.2)]
 connListRetLBlockerR = [(0, 4, -22.5, 0.2), (1, 5, -22.5, 0.2), (2, 6, -22.5, 0.2), (3, 7, -22.5, 0.2)]
-connListRetRBlockerL = [(0, 0, 22.5, 0.2), (1, 1, 22.5, 0.2), (2, 2, 22.5, 0.2), (3, 3, 22.5, 0.2)]
-connListRetRBlockerR = [(0, 4, -22.5, 0.2), (1, 5, -22.5, 0.2), (2, 6, -22.5, 0.2), (3, 7, -22.5, 0.2)]
+connListRetRBlockerL = [(0, 0, -22.5, 0.2), (1, 1, -22.5, 0.2), (2, 2, -22.5, 0.2), (3, 3, -22.5, 0.2)]
+connListRetRBlockerR = [(0, 4, 22.5, 0.2), (1, 5, 22.5, 0.2), (2, 6, 22.5, 0.2), (3, 7, 22.5, 0.2)]
 
 ps.Projection(pixel_r, collector, ps.OneToOneConnector(weights=20.5, delays=1.6), target='excitatory')
 ps.Projection(pixel_r, blockers, ps.FromListConnector(connListRetRBlockerR), target='excitatory')
@@ -34,6 +39,7 @@ ps.run(1000)
 spikes_c = collector.getSpikes()
 spikes_b = blockers.getSpikes()
 
-print(spikes_c, spikes_b)
+print(spikes_c)
+print(spikes_b)
 
 ps.end()
