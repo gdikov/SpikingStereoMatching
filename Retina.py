aaedd416154e5f052191c3acc6b06fdfe9cc90e8
Georgi Dikov <gvdikov@gmail.com>
2016-10-25 20:01:21 +0200

    record retina spikes too

--- Retina.py
+++ Retina.py
@@ -4,10 +4,13 @@
 # email: gvdikov93@gmail.com
 ###
 
+import os
+
 class Retina(object):
     def __init__(self, label="Retina", dimension_x=1, dimension_y=1,
                  use_prerecorded_input=True, spike_times=None,
-                 min_disparity=0, record_spikes=False, verbose=False):
+                 min_disparity=0, experiment_name=None,
+                 record_spikes=False, verbose=False):
         assert len(spike_times) >= dimension_x and len(spike_times[0]) >= dimension_y, \
             "ERROR: Dimensionality of retina's spiking times is bad. Retina initialization failed."
 
@@ -16,7 +19,7 @@ class Retina(object):
                   "This may lead to nonsensical results or failure as the network is not tested thoroughly."
 
         self.label = label
-
+        self.experiment_name = experiment_name
         self.dim_x = dimension_x
         self.dim_y = dimension_y
         self.use_prerecorded_input = use_prerecorded_input
@@ -68,3 +71,31 @@ class Retina(object):
                 if record_spikes:
                     col_of_pixels.record()
 
+    def get_spikes(self, sort_by_time=True, save_spikes=True):
+        spikes_per_population = [x[1].getSpikes() for x in self.pixel_columns]
+        spikes = list()
+        for col_index, col in enumerate(spikes_per_population, 0):  # it is 0-indexed
+            # for each spike in the population extract the timestamp and x,y coordinates
+            x_coord = col_index
+            for spike in col:
+                y_coord = int(spike[0])
+                spikes.append((round(spike[1], 1), x_coord+1, y_coord+1))	# pixel coordinates are 1-indexed
+        if sort_by_time:
+            spikes.sort(key=lambda x: x[0])
+        if save_spikes:
+            if not os.path.exists("./spikes"):
+                os.makedirs("./spikes")
+            i = 0
+            while os.path.exists("./spikes/{0}_{1}_spikes_{2}.dat".format(self.experiment_name, i, self.label)):
+                i += 1
+            with open('./spikes/{0}_{1}_spikes_{2}.dat'.format(self.experiment_name, i, self.label), 'w') as fs:
+                fs.write("### DATA FORMAT ###\n"
+                        "# Description: These are the spikes a retina has produced (see file name for exact retina label).\n"
+                        "# Each row contains: "
+                        "Time stamp -- x-coordinate -- y-coordinate\n"
+                        "### DATA START ###\n")
+                for s in spikes:
+                    fs.write(str(s[0]) + " " + str(s[1]) + " " + str(s[2]) + "\n")
+                fs.write("### DATA END ###")
+                fs.close()
+        return spikes
--- StereoVision.py
+++ StereoVision.py
@@ -12,29 +12,31 @@ import Simulation as sim
 
 if __name__ == "__main__":
 
-    experiment_name = "FanInputTest"
+    experiment_name = "SmallTest"
     experiment_duration = 10000
-    dx = 64
-    dy = 64
-    max_d = 16
+    dx = 5
+    dy = 5
+    max_d = 4
 
     # Setup the simulation
     Simulation = sim.SNNSimulation(simulation_time=experiment_duration)
 
     # Define the input source
     ExternalRetinaInput = \
-        eir.ExternalInputReader(file_path="input_data/NSTlogo_disp12-8-3.dat",
+        eir.ExternalInputReader(file_path="input_data/Small_input_test.dat",
                                 dim_x=dx,
                                 dim_y=dy,
-                                crop_window=True,
+                                crop_window=False,
                                 sim_time=experiment_duration)
 
     # Create two instances of Retinas with the respective inputs
     RetinaL = ret.Retina(label="RetL", dimension_x=dx, dimension_y=dy,
-                         spike_times=ExternalRetinaInput.retinaLeft)
+                         spike_times=ExternalRetinaInput.retinaLeft,
+                         experiment_name=experiment_name)
     RetinaR = ret.Retina(label="RetR", dimension_x=dx, dimension_y=dy,
-                         spike_times=ExternalRetinaInput.retinaRight)
-#    RetinaL = ret.Retina(label="RetL", dimension_x=dx, dimension_y=dy,
+                         spike_times=ExternalRetinaInput.retinaRight,
+                         experiment_name=experiment_name)
+   # RetinaL = ret.Retina(label="RetL", dimension_x=dx, dimension_y=dy,
 #                         spike_times=[[1, 25, 26, 27, 40]])
 #    RetinaR = ret.Retina(label="RetR", dimension_x=dx, dimension_y=dy,
 #                         spike_times=[[9, 41]])
@@ -51,6 +53,8 @@ if __name__ == "__main__":
 
     # Store the results in a file
     disparities = SNN_Network.get_spikes(sort_by_time=True, save_spikes=True)
+    ret_left_spikes = RetinaL.get_spikes(sort_by_time=True, save_spikes=True)
+    ret_right_spikes = RetinaR.get_spikes(sort_by_time=True, save_spikes=True)
 #    membrane_potential = SNN_Network.get_v(save_v=True)
 #     print(disparities)
 #    print(membrane_potential)
