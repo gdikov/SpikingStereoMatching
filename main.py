56e4aeee4d9af326dd47a5c9460c4f6b7245a98b
Georgi Dikov <gvdikov@gmail.com>
2016-11-23 20:38:32 +0100

    pendulum coords fixed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from examples import *
 
 if __name__ == "__main__":
+
+    run_experiment_pendulum()
+    # run_experiment_fans()
     # run_experiment_nst()
-    # run_experiment_pendulum()
-    run_experiment_fans()
