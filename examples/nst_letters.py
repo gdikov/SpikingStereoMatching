42537ee8bf8a94192fa77d3e901f29f1c3dd87a6
tophensen <c.richter@tum.de>
2017-04-27 12:38:15 +0200

    automatically figure out machine size before setup

--- examples/nst_letters.py
+++ examples/nst_letters.py
@@ -19,9 +19,13 @@ def run_experiment_nst(with_visualization=False):
     max_d = 24  # in pixels
     crop_xmin = 40  # in pixels
     crop_ymin = 50  # in pixels
+    
+    n_pops = 3* (2 * (dx) * (self.max_d + 1)
+                     - (self.max_d + 1) ** 2
+                     + self.max_d + 1) / 2
 
     # Setup the simulation
-    Simulation = SNNSimulation(simulation_time=experiment_duration)
+    Simulation = SNNSimulation(simulation_time=experiment_duration,n_chips_required=n_pops/16)
 
     # Define the input source
     path_to_input = os.path.join(os.path.dirname(os.path.realpath(__file__)),
--- network/simulation.py
+++ network/simulation.py
@@ -7,13 +7,14 @@
 import spynnaker.pyNN as ps
 
 class SNNSimulation(object):
-    def __init__(self, simulation_time=1000, simulation_time_step=0.2, threads_count=4):
+    def __init__(self, simulation_time=1000, simulation_time_step=0.2, n_chips_required=48*6, threads_count=4):
         self.simulation_time = simulation_time
         self.time_step = simulation_time_step
         # setup timestep of simulation and minimum and maximum synaptic delays
         ps.setup(timestep=simulation_time_step,
                  min_delay=simulation_time_step,
                  max_delay=10*simulation_time_step,
+                 n_chips_required = n_chips_required,
                  threads=threads_count)
 
     def run(self):
